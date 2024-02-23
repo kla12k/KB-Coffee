bdc42e155a8de5775c29cdfdd7aa6e8bef2fe0b3
kla12k <kaleabshewa456@gmail.com>
2024-02-23 17:16:59 +0300

    home

--- kb_coffee/lib/pages/home_screen.dart
+++ kb_coffee/lib/pages/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:passwordfield/passwordfield.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -7,7 +6,36 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: IconButton(
+              iconSize: 50,
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg2.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: IconButton(
+                iconSize: 35,
+                icon: Icon(Icons.notifications),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
         body: Container(
             height: MediaQuery.of(context).size.height * 6,
             width: MediaQuery.of(context).size.width * 6,
