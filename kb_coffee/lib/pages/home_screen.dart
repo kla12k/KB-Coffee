a9d7163ac3a6f9c307699dae0c17444a21703296
kla12k <kaleabshewa456@gmail.com>
2024-02-23 16:59:40 +0300

    home

--- kb_coffee/lib/pages/home_screen.dart
+++ kb_coffee/lib/pages/home_screen.dart
@@ -7,6 +7,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(),
         body: Container(
             height: MediaQuery.of(context).size.height * 6,
             width: MediaQuery.of(context).size.width * 6,
