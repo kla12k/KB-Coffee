8a9775423f81092882277de2f81a14d0ad1ac1c2
kla12k <kaleabshewa456@gmail.com>
2024-02-23 17:18:12 +0300

    appbar

--- kb_coffee/lib/pages/fav_screen.dart
+++ kb_coffee/lib/pages/fav_screen.dart
@@ -7,6 +7,36 @@ class FavScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 15.0),
+            child: IconButton(
+              iconSize: 50,
+              icon: Icon(
+                Icons.account_circle,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/bg2.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15.0),
+              child: IconButton(
+                iconSize: 35,
+                icon: Icon(Icons.notifications),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
         body: Container(
             height: MediaQuery.of(context).size.height * 6,
             width: MediaQuery.of(context).size.width * 6,
